eda9081708bb3b17854bb13b113f54de4e10bf0f
cbovens <collinbovenschen03@gmail.com>
2024-04-29 14:27:46 -0500

    LFSR innit

---
+++ src/cache/cacheLFSR.sv
@@ -0,0 +1,24 @@
+// LFSR Collin Bovenschen Koby Goree
+
+module cacheLFSR
+  #(parameter NUMWAYS = 4, SETLEN = 9, OFFSETLEN = 5, NUMLINES = 128) (
+  input  logic                clk, 
+  input  logic                reset,
+  input  logic                FlushStage,
+  input  logic                CacheEn,         // Enable the cache memory arrays.  Disable hold read data constant
+  input  logic [NUMWAYS-1:0]  HitWay,          // Which way is valid and matches PAdr's tag
+  input  logic [NUMWAYS-1:0]  ValidWay,        // Which ways for a particular set are valid, ignores tag
+  input  logic [SETLEN-1:0]   CacheSetData,    // Cache address, the output of the address select mux, NextAdr, PAdr, or FlushAdr
+  input  logic [SETLEN-1:0]   CacheSetTag,     // Cache address, the output of the address select mux, NextAdr, PAdr, or FlushAdr
+  input  logic [SETLEN-1:0]   PAdr,            // Physical address 
+  input  logic                LRUWriteEn,      // Update the LRU state
+  input  logic                SetValid,        // Set the dirty bit in the selected way and set
+  input  logic                ClearValid,      // Clear the dirty bit in the selected way and set
+  input  logic                InvalidateCache, // Clear all valid bits
+  output logic [NUMWAYS-1:0]  VictimWay        // LRU selects a victim to evict
+  );
+
+  
+
+
+endmodule
