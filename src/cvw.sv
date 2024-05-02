42904c270034437a2bf4684ad908b5a01cbbeda2
cbovens <collinbovenschen03@gmail.com>
2024-05-02 14:47:29 -0500

    cvw fix

--- src/cvw.sv
+++ src/cvw.sv
@@ -81,9 +81,11 @@ typedef struct packed {
   int           DCACHE_NUMWAYS;
   int           DCACHE_WAYSIZEINBYTES;
   int           DCACHE_LINELENINBITS;
+  int           DCACHE_SWITCH;
   int           ICACHE_NUMWAYS;
   int           ICACHE_WAYSIZEINBYTES;
   int           ICACHE_LINELENINBITS;
+  int           ICACHE_SWITCH;
   int           CACHE_SRAMLEN;
 
 // Integer Divider Configuration
